d9cbc678baa038e4e6df48bfeac93df07674c959
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2026-01-21 22:58:46 +0530

    footer updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,14 +3,15 @@ import './footer.scss';
 const Footer = () => {
     return (
         <footer>
+            <div className="icon sheryians"><img src="/doc-icons/sheryians.svg" alt="Sheryians icon" /></div>
+            <div className="icon youtube"><img src="/doc-icons/youtube.svg" alt="Youtube icon" /></div>
             <div className="icon github"><img src="/doc-icons/github.svg" alt="Github icon" /></div>
+            <div className="icon discord"><img src="/doc-icons/discord.svg" alt="Discord icon" /></div>
+            <div className="icon whatsapp"><img src="/doc-icons/whatsapp.svg" alt="Whatsapp icon" /></div>
             <div className="icon note"><img src="/doc-icons/note.svg" alt="Note icon" /></div>
             <div className="icon mail"><img src="/doc-icons/mail.svg" alt="Mail icon" /></div>
             <div className="icon pdf"><img src="/doc-icons/pdf.svg" alt="Pdf icon" /></div>
-            <div className="icon spotify"><img src="/doc-icons/spotify.svg" alt="Spotify icon" /></div>
             <div className="icon cli"><img src="/doc-icons/cli.svg" alt="Cli icon" /></div>
-            <div className="icon link"><img src="/doc-icons/link.svg" alt="Link icon" /></div>
-            <div className="icon calender"><img src="/doc-icons/calender.svg" alt="Calender icon" /></div>
         </footer>
     )
 }
